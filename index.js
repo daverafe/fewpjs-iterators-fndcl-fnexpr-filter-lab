657aa2a6aa804d4571ed51678ade2012260e9281
David Rafe <david.rafe@gmail.com>
2021-06-04 18:01:40 -0400

    commit tree ad1ada88a04b969cc637d7e4e29f488a9a0f7cd0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(drivers, name) {
+   let match = drivers.filter(driver => {
+       if(driver == name || driver.localeCompare(name, undefined, {sensitivity: 'base'}) == 0)
+        return driver 
+    })
+    return match 
+}
+
+function fuzzyMatch(drivers, letters) {
+    let match = drivers.filter(driver => {
+        let names = driver[0] + driver[1]
+        if(names === letters) {
+            return driver 
+        }
+    })
+    return match 
+}
+
+function matchName(drivers, name) {
+    let match = drivers.filter(driver => {
+        if(driver.name === name) {
+            return driver
+        }
+    })
+    return match 
+}
